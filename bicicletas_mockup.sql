58afc1d18837c71c56950d377a7ceb19936a7b60
Karolina Badilla <karolina14badilla@gmail.com>
2023-03-26 21:40:08 -0700

    actualizar imagenes

--- bicicletas_mockup.sql
+++ bicicletas_mockup.sql
@@ -23,18 +23,18 @@ CREATE TABLE bicicletas (
   marca VARCHAR(50) NOT NULL,
   modelo VARCHAR(50) NOT NULL,
   tamano VARCHAR(20) NOT NULL,
-  imagen VARCHAR(200) NOT NULL,
+  imagen VARCHAR(200) NOT NULL
 );
 
 INSERT INTO bicicletas (marca, modelo, tamano, imagen) VALUES
-  ('Specialized', 'Rockhopper', 'M', 'https://www.rei.com/media/product/1599240012'),
-  ('Trek', 'FX Sport 4', 'L', 'https://www.rei.com/media/product/136074'),
-  ('Giant', 'TCR Advanced Pro 1', 'S', 'https://www.rei.com/media/product/163215'),
-  ('Cannondale', 'Synapse Carbon Disc 105', 'L', 'https://www.rei.com/media/product/1510530016'),
-  ('Raleigh', 'Cadent 3', 'M', 'https://www.rei.com/media/product/1313380015'),
-  ('Fuji', 'Gran Fondo 2.1 Disc', 'S', 'https://www.rei.com/media/product/156645'),
-  ('Diamondback', 'Trace ST', 'L', 'https://www.rei.com/media/product/152485'),
-  ('Salsa', 'Warbird Carbon 105 700', 'M', 'https://www.rei.com/media/product/155300');
+  ('Specialized', 'Rockhopper', 'M', 'https://images.immediate.co.uk/production/volatile/sites/21/2021/04/Specialized-Rockhopper-Comp-hardtail-mountain-bike-01-300dff1.jpg?quality=90&resize=620%2C413'),
+  ('Trek', 'FX Sport 4', 'L', 'https://i.ytimg.com/vi/MCj0g59upz8/maxresdefault.jpg'),
+  ('Giant', 'TCR Advanced Pro 1', 'S', 'https://bikestore.com.mx/blog/wp-content/uploads/2021/02/P1012949-1d.jpg'),
+  ('Cannondale', 'Synapse Carbon Disc 105', 'L', 'https://images.immediate.co.uk/production/volatile/sites/21/2023/01/C23SynapseAlloy-hero-a8e8b68.jpg?quality=45&resize=768,574'),
+  ('Raleigh', 'Cadent 3', 'M', 'https://www.sefiles.net/images/library/large/raleigh-cadent-3-361393-18.png'),
+  ('Fuji', 'Gran Fondo 2.1 Disc', 'S', 'https://cdn.road.cc/sites/default/files/fuji-gran-fondo-1.1-glamour-shot.jpg'),
+  ('Diamondback', 'Trace ST', 'L', 'https://m.media-amazon.com/images/I/5179j7XygkL._AC_.jpg'),
+  ('Salsa', 'Warbird Carbon 105 700', 'M', 'https://s3.amazonaws.com/www.bikerumor.com/wp-content/uploads/2018/09/02155216/2019-Salsa-Warbird-v4_lightweight-650B-700C-carbon-gravel-road-race-bike_105-complete.jpg');
 
 
 -- Crear la tabla de transacciones
