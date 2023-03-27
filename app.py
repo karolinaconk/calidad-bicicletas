e3184a6f59c7c138cb6a637a65dd2439f3864136
Karolina Badilla <karolina14badilla@gmail.com>
2023-03-26 23:19:20 -0700

    sign up, log in and log out ready!!

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def signup():
                 correo = request.form['email']
                 phone = request.form['phone']
                 identification = request.form['identification']
-                password = sha256_crypt.hash(request.form['password'])
+                password = request.form['password']
                 registrado = agregar_usuario(dicc_usuarios,nombre,correo,phone,password,identification,'activo',config)
                 if registrado:
                     return render_template('login.html')
@@ -116,29 +116,12 @@ def alquilar():
         conexion.close()
         return render_template('alquilar.html', bicicletas=bicicletas)
 
-# Ruta para iniciar sesión
-'''@app.route('/iniciar_sesion', methods=['GET', 'POST'])
-def iniciar_sesion():
-    if request.method == 'POST':
-        # Lógica para verificar las credenciales del usuario y crear la sesión
-        return redirect(url_for('index'))
-    else:
-        return render_template('iniciar_sesion.html')'''
+@app.route("/logout", methods=['GET','POST'])
+def logout():
+    if request.method == 'GET':
+        session['logged_in'] = False
+        return redirect('/')
 
-'''# Ruta para registrarse
-@app.route('/registrarse', methods=['GET', 'POST'])
-def registrarse():
-    if request.method == 'POST':
-        # Lógica para crear un nuevo usuario en la base de datos
-        return redirect(url_for('index'))
-    else:
-        return render_template('registrarse.html')'''
-
-# Ruta para cerrar sesión
-@app.route('/cerrar_sesion')
-def cerrar_sesion():
-    # Lógica para cerrar la sesión del usuario
-    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
